8707f13cbe4be14da9b180887b86d069971d0a8f
rlomeli <parapalapa@gmail.com>
2013-10-24 02:52:49 -0500

    move layout basic, more results

--- SalarioMin/src/com/example/salariomin/MainActivity.java
+++ SalarioMin/src/com/example/salariomin/MainActivity.java
@@ -18,12 +18,19 @@ public class MainActivity extends Activity implements OnItemSelectedListener{
     EditText num1;
 	Button button01;
 	TextView tv1;
+	
+	TextView tv1b;
+	TextView tv2b;
+	TextView tv3b;
+	TextView tv4b;
+	
 	ImageView iv1;
 	Spinner spinOps;
-	String[] ops={"Area Geografica ? ","Area A 64.76","Area B 61.38"};
+	
+	String[] ops={"Lugar","Mexico Area A 64.76","Mexico Area B 61.38", "Nueva York", "Tu Salario"};
 	//String[] ops={"Choose operation","+","-","*","/"};
 	String opselected;
-	double a, c;
+	double a, c, d, e, f, g;
     private OnClickListener myClickListener = new OnClickListener()
     {
     	public void onClick(View v) {
@@ -32,8 +39,40 @@ public class MainActivity extends Activity implements OnItemSelectedListener{
     		//if (opselected=="+"){c=a+b; tv1.setText(Double.toString(c));}
     		
     		
-    		if (opselected=="Area A 64.76"){c=a/64.76; tv1.setText(Double.toString(c));}
-    		else if (opselected=="Area B 61.38"){c=a/61.38; tv1.setText(Double.toString(c));}
+    		if (opselected=="Mexico Area A 64.76"){c=a/64.76; tv1.setText(Double.toString(c));}
+    		//else if (opselected=="Mexico Area B 61.38"){c=a/61.38; tv1.setText(Double.toString(c));}
+    		
+    		else if (opselected=="Mexico Area B 61.38"){
+    			c=a/61.38; tv1.setText(String.format("%.2f", c));
+    			d=a*2; tv1b.setText(String.format("%.2f", d));
+    			e=a*3; tv2b.setText(String.format("%.2f", e));
+    			f=a*4; tv3b.setText(String.format("%.2f", f));
+    			g=a*5; tv4b.setText(String.format("%.2f", g));
+    			}  
+    		
+    		//else if (opselected=="Mexico Area B 61.38"){c=a/61.38; tv1.setText( String.format( "Value of a: %.2f", a ) );}
+    		
+    		
+    		
+    		else if (opselected=="Nueva York"){c=a*2; tv3b.setText(Double.toString(c));}
+    		else if (opselected=="Tu Salario"){c=a*3; tv4b.setText(Double.toString(c));}
+    		
+    		// con decimanles
+    		//Text.setText( String.format( "Value of a: %.2f", a ) );
+    		// You can use a DecimalFormat, or String.format("%.2f", a);
+    		
+    	// original	
+    	//	if (opselected=="+"){c=a+b; tv1.setText(Double.toString(c));}
+    	//	else if (opselected=="-"){c=a-b; tv1.setText(Double.toString(c));}
+    	//	else if (opselected=="*"){c=a*b; tv1.setText(Double.toString(c));}
+    	//	else if (opselected=="/"){c=a/b; tv1.setText(Double.toString(c));}
+    	//	else {tv1.setText("Please select the operation");}
+    		
+    		
+    		
+    		
+    		
+    		
     		
     		
     		else {tv1.setText("Please select the operation");}
@@ -45,9 +84,21 @@ public class MainActivity extends Activity implements OnItemSelectedListener{
         @Override
         public void onCreate(Bundle savedInstanceState) {
            super.onCreate(savedInstanceState);
-           setContentView(R.layout.activity_main);
+           setContentView(R.layout.activity_main_dos);
             
            tv1 = (TextView)findViewById(R.id.TextView01);
+           
+           tv1b = (TextView)findViewById(R.id.textView1b);
+           tv2b = (TextView)findViewById(R.id.textView2b);
+           tv3b = (TextView)findViewById(R.id.textView3b);
+           tv4b = (TextView)findViewById(R.id.textView4b);
+           
+
+           
+           
+           
+           
+           
            //iv1= (ImageView)findViewById(R.id.);//try imageview
            
            button01 = (Button)findViewById(R.id.Button01);
@@ -70,7 +121,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener{
         }   
         public void onItemSelected (AdapterView<?> p,View v,int position,long id) {
         	opselected=ops[position];
-        	tv1.setText("Elegiste " + opselected);
+        	//tv1.setText("Elegiste " + opselected);
         }
         public void onNothingSelected(AdapterView<?> p) { 
         	tv1.setText("Please select ops"); 
